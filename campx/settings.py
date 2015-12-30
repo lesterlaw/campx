d4f1c72f98a8c4410407bd0ecac5fb47e5637c17
Lester <lesterlaw7798@gmail.com>
2015-12-30 14:56:33 +0800

    5

--- campx/settings.py
+++ campx/settings.py
@@ -104,9 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REGISTRATION_OPEN = True        # If True, users can register
