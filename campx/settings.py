a1871cceda73d1ea0f7a0d0342871e9619861ab7
Lester <lesterlaw7798@gmail.com>
2015-12-30 14:52:20 +0800

    third

--- campx/settings.py
+++ campx/settings.py
@@ -115,3 +115,4 @@ REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged
 LOGIN_REDIRECT_URL = '/wishcamps/'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/' # The page users are directed to if they are not logged in,
                                                                 # and are trying to access pages requiring authentication
+PROJECT_DIR  = os.path.dirname(__file__)
