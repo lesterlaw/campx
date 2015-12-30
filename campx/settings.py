82d2319376a6123753af9a43403426a027073229
Lester <lesterlaw7798@gmail.com>
2015-12-30 14:54:11 +0800

    fouth

--- campx/settings.py
+++ campx/settings.py
@@ -107,7 +107,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REGISTRATION_OPEN = True        # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
@@ -115,4 +115,3 @@ REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged
 LOGIN_REDIRECT_URL = '/wishcamps/'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/' # The page users are directed to if they are not logged in,
                                                                 # and are trying to access pages requiring authentication
-PROJECT_DIR  = os.path.dirname(__file__)
